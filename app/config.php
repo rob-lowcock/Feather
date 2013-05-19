80233a81b31af239a5bbaf7726b160ad8e4b898d
Rob Lowcock <rob.lowcock@gmail.com>
2013-05-19 11:35:16 +0100

    Added readme and public config option

--- app/config.php
+++ app/config.php
@@ -1,5 +1,9 @@
 <?php
 
+// The default controller accessed at the root of your site
 define('DEFAULT_CONTROLLER', 'example');
+
+// The name of the directory containing index.php
+define('PUBLIC_FOLDER', 'public')
 
 ?>
--- app/controllers/example.php
+++ app/controllers/example.php
@@ -1,14 +1,28 @@
 <?php
-
+/**
+ * Example controller
+ *
+ * The class name MUST be the same as the filename, but with the first letter capitalised.
+ * This class deals with all pages and sub-pages at yoursite.com/example/
+ */
 class Example extends BaseController {
 	
+	/**
+	 * Index page.
+	 *
+	 * Returns the default view
+	 * Accessible at yoursite.com/example/
+	 * 
+	 * @return void
+	 */
 	public function index()
 	{
-		$stuff = Model::load('example/stuff');
+		// The following commented lines load a model and execute a function from that model
+		
+		// $stuff = Model::load('example/stuff');
+		// echo $stuff->do_stuff();
 
-		echo $stuff->do_stuff();
-
-		//View::load('feather/default');
+		View::load('feather/default');
 	}
 
 }
